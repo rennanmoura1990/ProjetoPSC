3177fdc4b2ff603f99ed4b4ac6e4e4e19b690790
rennanmoura1990 <ramf1777@gmail.com>
2016-04-16 23:48:48 -0300

    Feita a modelagem objeto-relacional

--- ProjetoPSC/src/model/Coordenador.java
+++ ProjetoPSC/src/model/Coordenador.java
@@ -1,5 +1,10 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+
+@Entity
+@PrimaryKeyJoinColumn(name="id_pessoa")
 public class Coordenador extends Pessoa {
 
 }
--- ProjetoPSC/src/model/Telefones.java
+++ ProjetoPSC/src/model/Telefones.java
@@ -1,7 +1,44 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+@Entity
 public class Telefones {
+	@Id
 	private int id_telefone;
+	@Column(nullable=false)
 	private String telefone;
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="id_pessoa",insertable=true,updatable=true)
+	@Fetch(FetchMode.JOIN)
+	@Cascade(CascadeType.SAVE_UPDATE)
 	private Pessoa pessoa;
+	public int getId_telefone() {
+		return id_telefone;
+	}
+	public void setId_telefone(int id_telefone) {
+		this.id_telefone = id_telefone;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+	
 }
