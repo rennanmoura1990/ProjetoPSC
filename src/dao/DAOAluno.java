5f9c413a76c81faca320d86d45595f3da8d428c2
rennanmoura1990 <ramf1777@gmail.com>
2016-04-23 19:31:14 -0300

    Simplificado a consulta de número de alunos,na classe DAOAluno

--- src/dao/DAOAluno.java
+++ src/dao/DAOAluno.java
@@ -2,34 +2,23 @@ package dao;
 
 import javax.persistence.PersistenceException;
 import javax.persistence.Query;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.Root;
 
 import exception.DAOException;
 import model.Aluno;
 
 public class DAOAluno extends DAOGenerico<Aluno> implements IDAOAluno {
-	
+
 	/**
 	 * Retorna a quantidade de alunos de uma determinada turma
+	 * 
 	 * @param turma_id
 	 * @return
 	 */
-	public long QtdAlunoTurma(int turma_id)  throws DAOException{
-		try{
-		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-		CriteriaQuery criteriaQuery = criteriaBuilder.createQuery();
-		Root aluno = criteriaQuery.from(Aluno.class);
-		criteriaQuery.select(criteriaBuilder.count(aluno));
-		Join turma = aluno.join("turma");
-		criteriaQuery.where(criteriaBuilder.equal(turma.get("id_turma"),turma_id));
-		Query query = em.createQuery(criteriaQuery);
-		return (long) query.getSingleResult();
-		}
-		catch (PersistenceException e) 
-		{
+	public long QtdAlunoTurma(int turma_id) throws DAOException {
+		try {
+			Query query = em.createQuery("SELECT COUNT (*) from Aluno a", Aluno.class);
+			return (long) query.getSingleResult();
+		} catch (PersistenceException e) {
 			throw new DAOException(e);
 		}
 	}
